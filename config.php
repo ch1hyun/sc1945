5f82b670704a94fc5d847cdbe77fafbcf81172dd
ch1hyun <och2510@gmail.com>
2021-10-17 23:46:03 +0900

    renewer

--- config.php
+++
@@ -1,5 +0,0 @@
-<?php
-function db_connect() {
-	return mysqli_connect("localhost", "root", "sh1945", "sc_letter");
-}
-?>
--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-if ($_SESSION['flag']) {
-	session_destroy();
-	echo "<script>location.href='./';</script>";
-} else {
-	echo "<script>location.href='./';</script>";
-}
-?>
